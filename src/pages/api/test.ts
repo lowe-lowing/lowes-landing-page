76185a542aa401a7f77a7f9e6fb3bf78b0e75eaa
Lowe Löwing <lowe.lowing@gmail.com>
2023-07-01 20:43:20 +0200

    base64

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -2,9 +2,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  const private_key = Buffer.from(process.env.PRIVATE_KEY as string, "base64").toString("ascii");
+  console.log(private_key);
+
   res.json({
     name: "John Doe",
     a: process.env.CLIENT_EMAIL,
-    b: process.env.PRIVATE_KEY,
+    b: private_key,
   });
 }
