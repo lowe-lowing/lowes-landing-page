3fe59f30388ea21f787f0fc457938073212df742
Lowe Löwing <lowe.lowing@gmail.com>
2023-07-01 19:59:18 +0200

    throw error

--- src/pages/api/resume.ts
+++ src/pages/api/resume.ts
@@ -8,20 +8,24 @@ const scopes = ["https://www.googleapis.com/auth/drive"];
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Buffer>) {
   console.log(process.env.CLIENT_EMAIL);
 
-  const auth = new google.auth.JWT(
-    process.env.CLIENT_EMAIL,
-    undefined,
-    process.env.PRIVATE_KEY,
-    scopes
-  );
-  const drive = google.drive({ version: "v3", auth });
-  const { id } = ResumeValidator.parse(req.body);
-  const cv = await drive.files.export(
-    {
-      fileId: id,
-      mimeType: "application/pdf",
-    },
-    { responseType: "arraybuffer" }
-  );
-  res.send(Buffer.from(cv.data as string));
+  try {
+    const auth = new google.auth.JWT(
+      process.env.CLIENT_EMAIL,
+      undefined,
+      process.env.PRIVATE_KEY,
+      scopes
+    );
+    const drive = google.drive({ version: "v3", auth });
+    const { id } = ResumeValidator.parse(req.body);
+    const cv = await drive.files.export(
+      {
+        fileId: id,
+        mimeType: "application/pdf",
+      },
+      { responseType: "arraybuffer" }
+    );
+    res.send(Buffer.from(cv.data as string));
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
