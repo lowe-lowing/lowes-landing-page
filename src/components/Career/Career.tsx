55833ed1b486915cf4bf18e668916aa81427b590
Lowe Löwing <lowe.lowing@gmail.com>
2024-06-29 17:28:49 +0200

    Update career information and add new education and certificates

--- src/components/Career/Career.tsx
+++ src/components/Career/Career.tsx
@@ -21,60 +21,97 @@ export const Career = () => {
                     description="This is my current position interning at Red River Consulting. We use Angular and C#/.NET where I work full stack in a team creating an advanced reporting system for firefighters and paramedics. Here I have learned how to work in a group, write good and future-proof code and focus on what is important to the customer."
                   />
                   <Employment
-                    company={"QTE Development"}
-                    position={"Fullstack Junior Developer"}
-                    dateDisplay={"Februari 2023 - April 2023"}
-                    description={
-                      "During 3 months I got to learn a lot that I had never used before, such as working more with github and setting up a professional development environment. I was both thrown into an existing project and had to start a new project where I developed my experiences in postgresql, typescript and nestjs, etc."
-                    }
-                    place={"Stockholm"}
+                    company="QTE Development"
+                    position="Fullstack Junior Developer"
+                    dateDisplay="Februari 2023 - April 2023"
+                    description="During 3 months I got to learn a lot that I had never used before, such as working more with github and setting up a professional development environment. I was both thrown into an existing project and had to start a new project where I developed my experiences in postgresql, typescript and nestjs, etc."
+                    place="Stockholm"
                   />
                 </div>
               </div>
               <div>
                 <CVTitle title="Education" />
-                <Education
-                  school={"Åva Gymnasium"}
-                  degree={"Tech"}
-                  dateDisplay={"Augusti 2019 - Juni 2022"}
-                  description={
-                    "During my high school years, I developed a great interest in programming and was often ahead of the curve in courses such as Programming, Web Development and Web Server Programming."
-                  }
-                  place={"Täby"}
-                />
+                <div className="space-y-2">
+                  <Education
+                    school="Åva Gymnasium"
+                    degree="Tech"
+                    dateDisplay="Augusti 2019 - Juni 2022"
+                    description="During my high school years, I developed a great interest in programming and was often ahead of the curve in courses such as Programming, Web Development and Web Server Programming."
+                    place="Täby"
+                  />
+                  <Education
+                    school="Medieinstitutet"
+                    degree="Mjukvarutestning"
+                    dateDisplay="April 2024 - June 2024"
+                    description=""
+                    place="Remote"
+                  />
+                  <Education
+                    school="IT-Högskolan"
+                    degree="Devops"
+                    dateDisplay="April 2024 - June 2024"
+                    description=""
+                    place="Remote"
+                  />
+                  <Education
+                    school="Blekinge Tekniska Högskolan"
+                    degree="Smart industri: automation och artificiell intelligens inom industri 4.0"
+                    dateDisplay="January 2024 - June 2024"
+                    description=""
+                    place="Remote"
+                  />
+                </div>
               </div>
               <div>
                 <CVTitle title="Certificates" />
                 <div className="flex flex-col gap-2">
                   <Certificate
-                    title={"HTML"}
-                    source={"Sololearn"}
-                    date={"Dec 2022"}
-                    link={"https://www.sololearn.com/certificates/CT-2IHYO59T"}
+                    title="Microsoft Certified: Azure AI Fundamentals"
+                    source="Microsoft Certified"
+                    date="June 2024"
+                    link="https://learn.microsoft.com/api/credentials/share/en-us/LoweLwing-8635/C56F159686D3335E?sharingId=F25743419594C0A2"
                   />
                   <Certificate
-                    title={"CSS"}
-                    source={"Sololearn"}
-                    date={"Dec 2022"}
-                    link={"https://www.sololearn.com/certificates/CT-Y6PIVCUK"}
+                    title="Google UX Design Specialization"
+                    source="Coursea"
+                    date="Mars 2024"
+                    link="https://coursera.org/share/f3b10c864a384af9e4f6d634b0ac56be"
                   />
                   <Certificate
-                    title={"JavaScript"}
-                    source={"Sololearn"}
-                    date={"Dec 2022"}
-                    link={"https://www.sololearn.com/certificates/CT-W9FL5ZJ1"}
+                    title="Microsoft Certified: Azure Fundamentals"
+                    source="Microsoft Certified"
+                    date="Mars 2024"
+                    link="https://learn.microsoft.com/api/credentials/share/en-us/LoweLwing-8635/7A634472776B2C5D?sharingId=F25743419594C0A2"
                   />
                   <Certificate
-                    title={"Responsive Web Design"}
-                    source={"Sololearn"}
-                    date={"Dec 2022"}
-                    link={"https://www.sololearn.com/certificates/CT-KFJ4EIBN"}
+                    title="HTML"
+                    source="Sololearn"
+                    date="Dec 2022"
+                    link="https://www.sololearn.com/certificates/CT-2IHYO59T"
                   />
                   <Certificate
-                    title={"React + Redux"}
-                    source={"Sololearn"}
-                    date={"Dec 2022"}
-                    link={"https://www.sololearn.com/certificates/CT-PZE0GA2L"}
+                    title="CSS"
+                    source="Sololearn"
+                    date="Dec 2022"
+                    link="https://www.sololearn.com/certificates/CT-Y6PIVCUK"
+                  />
+                  <Certificate
+                    title="JavaScript"
+                    source="Sololearn"
+                    date="Dec 2022"
+                    link="https://www.sololearn.com/certificates/CT-W9FL5ZJ1"
+                  />
+                  <Certificate
+                    title="Responsive Web Design"
+                    source="Sololearn"
+                    date="Dec 2022"
+                    link="https://www.sololearn.com/certificates/CT-KFJ4EIBN"
+                  />
+                  <Certificate
+                    title="React + Redux"
+                    source="Sololearn"
+                    date="Dec 2022"
+                    link="https://www.sololearn.com/certificates/CT-PZE0GA2L"
                   />
                 </div>
               </div>
